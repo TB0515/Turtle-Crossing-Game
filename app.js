fc710dbbd8081713eb233d7016ff0d0a0ebdb466
TB <thrishbg15@gmail.com>
2025-08-29 00:17:28 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// Objective: Guide a player across a busy road while avoiding obstacles (car). 
+// The goal is to safely reach the other side without getting hit.
+// Features:As level increases, car speed gets faster. Score is tracked. No saves. Make it responsive.
+// Player can only move forward (bottom to top - so, event listener for up arrow)
+// Cars are in diff color, start at random yaxis, move from right to left.
+// TO DO 0: create, export and import car,  player, scoreboard modules to app. 
+// TO DO 1: Create Player and keyboard controls.
+// TO DO 2: Initial Cars, Remove cars that goes off-screen and create new cars at random intervals.
+// TO DO 3: Checkcollion an reset the canvas.
+// TO DO 4: Track score and level complete.
+// TO DO 5: End the loop 
+
+
+
+
